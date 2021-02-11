981151ea14521c2196b62ecd2be894f300d73431
Karla Dayhoff <kddayhoff@gmail.com>
2021-02-11 11:01:23 -0600

    incorrectfunctionality in server file

--- server.js
+++ server.js
@@ -15,9 +15,15 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+
 // Connect to the Mongo DB
-mongoose
-    .connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooksearch', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooksearch', {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
